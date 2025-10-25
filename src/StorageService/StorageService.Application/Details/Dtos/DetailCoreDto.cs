000d1d15c7f2ba229e0876b88e6245ba4d1410ac
Ilya Rabets <ilyarabets@gmail.com>
2025-10-25 19:12:59 +0300

    Update detail core dto entity

--- src/StorageService/StorageService.Application/Details/Dtos/DetailCoreDto.cs
+++ src/StorageService/StorageService.Application/Details/Dtos/DetailCoreDto.cs
@@ -4,12 +4,13 @@ namespace StorageService.Application.Details.Dtos;
 
 public record DetailCoreDto
 {
+    public int Id { get; set; }
     public string NomenclatureCode { get; set; }
     public string Name { get; set; }
     public int Count { get; set; }
     public int StorekeeperId { get; set; }
     public StorekeeperCoreDto? Storekeeper { get; set; }
     public bool IsDeleted { get; set; }
-    public DateTime CreatedAtUtc { get; set; }
-    public DateTime? DeletedAtUtc { get; set; }
+    public DateTime CreatedAtDate { get; set; }
+    public DateTime? DeletedAtDate { get; set; }
 }
